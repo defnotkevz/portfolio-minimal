72db7b766df5f7dabf80f664fed2b2b7e972abc7
Kevin Roy <kevnroy1@gmail.com>
2025-02-17 21:14:46 +0530

    testing commits

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
+      <div>
+        <h1 className="text-2xl font-semibold tracking-tighter">Kevin Roy</h1>
+        <h3 className="mb-8">Full Stack Engineer</h3>
+      </div>
+
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        {/* {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        mode, which eases long coding sessions by reducing eye strain.`} */}
+        {`I love beautiful software.`}
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
